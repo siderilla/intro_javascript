3670d603fc4989e4dfb8ea8a90c11d73629cac59
siderilla <siderilla11@gmail.com>
2025-02-24 23:28:51 +0100

    ok

--- exercises-0.js
+++ exercises-0.js
@@ -6,13 +6,17 @@
 // * * * *
 // # # # # #
 
-for (let i = 1; i <= 5; i++) {
-	if (i % 2 === 0) {
-		console.log("* *");
-	} else {
-		console.log("# ");
-	}
-}
+// for (let i = 1; i <= 5; i++) {
+// 	let row = "";
+// 	for (let j = 1; j < i; j++) {
+// 		if (i % 2 === 0) {
+// 			row += "* ";
+// 		} else {
+// 			row += "# ";
+// 		}		
+// 	}
+// 	console.log(row);
+// }
 
 // i = 0;
 // while (i < 5) {
@@ -29,6 +33,32 @@ for (let i = 1; i <= 5; i++) {
     // 4 4 4 4
     // 5 5 5 5 5
 
+// for (let i = 1; i <= 5; i++) {
+// 	let row = "";
+// 	for (let j = 0; j < i; j++) {
+// 		if (i === 1) {
+// 			row += "1";
+// 		} else if (i === 2) {
+// 			row += "2 ";
+// 		} else if (i === 3) {
+// 			row += "3 ";
+// 		} else if (i === 4) {
+// 			row += "4 ";
+// 		} else {
+// 			row += "5 ";
+// 		}		
+// 	}
+// 	console.log(row);
+// }
+
+// for (let i = 1; i <= 5; i++) {
+// 	let row = "";
+// 	for (let j = 0; j < i; j++) {
+// 		row += i + " ";	
+// 	}
+// 	console.log(row);
+// }
+
 ////////////////////////// 3 //////////////////////////
 // Piramide di Numeri Pari:
 // Crea una piramide dove ogni riga contiene numeri pari. Ad esempio:
@@ -37,6 +67,14 @@ for (let i = 1; i <= 5; i++) {
     // 6 6 6
     // 8 8 8 8
     // 10 10 10 10 10
+
+// for (let i = 1; i <= 5; i++) {
+// 	let row = "";
+// 	for (let j = 0; j < i; j++) {
+// 		row += (i * 2) + " ";	
+// 	}
+// 	console.log(row);
+// }
 
 ////////////////////////// 4 //////////////////////////
 // Piramide di Fibonacci:
@@ -49,6 +87,12 @@ for (let i = 1; i <= 5; i++) {
 ////////////////////////// 5 //////////////////////////
 // Log dei Numeri Primi:
 // Usa un ciclo per loggare tutti i numeri primi da 1 a 100.
+
+// for (let i = 2; i <= 100; i++) {
+// 	if (i / i === 1 && i / 1 === i) {
+// 		console.log(i);
+// 	}
+// }
 
 ////////////////////////// 6 //////////////////////////
 // Log dei Numeri Divisibili per 3 o 5:
@@ -89,3 +133,29 @@ for (let i = 1; i <= 5; i++) {
 ////////////////////////// 15 //////////////////////////
 // Log dei Numeri con Cifre Speculari:
 // Usa un ciclo per loggare tutti i numeri da 1 a 100 che sono speculari (es. 12 e 21, 13 e 31, ecc.).
+
+
+////////////////////////// CODEWARS //////////////////////////
+// SUM NUMBERS
+
+// function sum(numbers = []) {
+// 	if (numbers.length === 0) {
+// 		return 0;
+// 	}
+
+// 	let sum = 0;
+// 	for (let i = 0; i < numbers.length; i++) {
+// 	  sum += numbers[i]; 
+// 	}
+// 	return sum;
+//   }
+  
+//   console.log(sum());
+//   console.log(sum([1, 5.2, 4, 0, -1]))
+
+// // VALIDA ALTERNATIVA:
+
+//   function sum(numbers) {
+// 	return numbers.reduce((a, b) => a + b, 0);
+//   }
+
