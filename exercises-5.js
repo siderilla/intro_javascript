8f46efd11e66cc0f9340d636adc9e9bba96e267e
siderilla <siderilla11@gmail.com>
2025-02-26 23:59:35 +0100

    ok

--- exercises-5.js
+++ exercises-5.js
@@ -1,8 +1,4 @@
-
-const array1 = [23, 14, 56, 3, 0, -1, 55, -12, 60, 8];
-
-///////////////////////  1  ////////////////////////
-// filtrare tutti i numeri minori o uguali a zero (FILTER)
+// ALL MY FUNCTIONS
 
 function filter(array, filteringFunction) {
     const parkingArray = [];
@@ -15,22 +11,52 @@ function filter(array, filteringFunction) {
     return parkingArray;
 }
 
-function isMinorEqualZero(nbr) {
-    if (nbr <= 0) {
-        return true;
-    } else {
-        return false;
+function reduce(array, reducingFunction, startingAccumulator) {
+    let accumulator = startingAccumulator;
+    for (let i = 0; i < array.length; i++) {
+        const current = array[i]; // elemento corrente su array
+        accumulator = reducingFunction(accumulator, current, i, array);
     }
+    return accumulator;
 }
 
-const minOrEqualZero = filter(array1, isMinorEqualZero); // chiamo filter() devo specificare com'è la funzione
+function map(array, mappingFunction) {
+    const parkingArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const newElement = mappingFunction(element, i, array);
+
+        parkingArray.push(newElement);
+    }
+    return parkingArray;
+}
+
+
+const array1 = [23, 14, 56, 3, 0, -1, 55, -12, 60, 8];
+
+///////////////////////  1  ////////////////////////
+// filtrare tutti i numeri minori o uguali a zero (FILTER)
+
+// versione brutta:
+
+// function isMinorEqualZero(nbr) {
+//     if (nbr <= 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const minOrEqualZero = filter(array1, isMinorEqualZero); // chiamo filter() devo specificare com'è la funzione
 // se voglio che chiami la funzione nella libreria allora la notazione è .filter
 
-// const minOrEqualZero = array1.filter(nbr => nbr <= 0);
+const minOrEqualZero = array1.filter(nbr => nbr <= 0);
 console.log ('filter numbers less or equal to zero', minOrEqualZero);
 
 ///////////////////////  2  ////////////////////////
 // convertire celsius in farhenheit (MAP)
+
+// versione brutta:
 
 // function convertionCelsius(num) {
 //     const newFarh = (num * 9/5) + 32;
@@ -43,47 +69,83 @@ console.log ('map celsius to far', convertCelsiusToFar);
 ///////////////////////  3  ////////////////////////
 // fare la media dei valori (REDUCE)
 
-function reduce(array, reducingFunction, startingAccumulator) {
-    let accumulator = startingAccumulator;
-    for (let i = 0; i < array.length; i++) {
-        const current = array[i]; // elemento corrente su array
-        accumulator = reducingFunction(accumulator, current, i, array);
-    }
-    return accumulator;
-}
+// versione brutta:
 
-function avarageArray(accumulator, current, array) {
-    const newAccumulator = accumulator + current;
-    const avarageAll = newAccumulator / array.length;
-    return avarageAll;
-}
+// function avarageArray(arrayOfNumbers) {
+//     let accumulator = 0;
+// 	for (let i = 0; i < arrayOfNumbers.length; i++) {
+// 		const current = arrayOfNumbers[i];
+// 		accumulator += current;
+// 	}
+//     return accumulator;
+// }
 
-const avarageAv = reduce(array1, avarageArray);
+// const sumAll = avarageArray(array1);
+// const avarageAv = sumAll / array1.length;
 
-
-// const avarageArray = array1.reduce((a, c) => (a + c), 0) / array1.length;
-console.log('media', avarageAv);
+const avarageAv = array1.reduce((a, c) => (a + c), 0) / array1.length;
+console.log('avarage of numbers:', avarageAv);
 
 
 ///////////////////////  4  ////////////////////////
 // restituire valore più basso e il più alto (REDUCE)
+
+const smallerAndGreaterValue = reduce(array1, function(acc, current) {
+    return [
+        Math.min(acc[0], current),
+        Math.max(acc[1], current)
+    ];
+}, [Infinity, -Infinity]);
+
+console.log('smaller and greater value:', smallerAndGreaterValue);
+
+// const smallerAndGreaterValue = array1.reduce((acc, current) => {
+//     return [
+//         Math.min(acc[0], current),
+//         Math.max(acc[1], current)
+//     ];
+// }, [Infinity, -Infinity]);
+
+// console.log('smaller and greater value:', smallerAndGreaterValue);
 
 const array2 = ['pippo', 'pluto', 'paperino', 'qui', 'quo', 'qua', 'clarabella', 'minnie']
 
 ///////////////////////  5  ////////////////////////
 // filtrare tutti i nomi più grandi di 4 char (FILTER)
 
+const filterNames = array2.filter(str => str.length > 4);
+console.log ('names greater than 4 chars:', filterNames);
+
 ///////////////////////  6  ////////////////////////
 // trasformare tutte le stringhe in maiuscolo (MAP)
+
+const stringToUpper = array2.map(function(str) {
+	return str.toUpperCase();
+});
+console.log('to upper case strings:', stringToUpper);
 
 ///////////////////////  7  ////////////////////////
 // mettere SOLO le iniziali maiuscole (MAP)
 
+const initialToUpper = array2.map(function(str) {
+	return str[0].toUpperCase() + str.slice(1);
+});
+console.log('to upper case initial:', initialToUpper);
+
 ///////////////////////  8  ////////////////////////
 // restituire una stringa composta dall'iniziale delle parole (REDUCE)
 
+const onlyInitials = array2.reduce((a, c) => a + c, "");
+console.log('only initial:', onlyInitials);
+
 ///////////////////////  9  ////////////////////////
 // sommare tutti i char di tutte le stringhe (unico return) (REDUCE)
+
+const sumCharsToNum = array2.reduce(function(acc, str) {
+    return acc + str.length;
+}, 0);
+
+console.log('total number of characters:', sumCharsToNum);
 
 ///////////////////////  10  ////////////////////////
 // eliminare le vocali dalle stringhe (MAP)
